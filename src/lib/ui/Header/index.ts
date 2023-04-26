a1b54833f68d52a0cc56d24f7c63da8c97c683df
MyeongJunKim <myeongjun222@naver.com>
2023-04-26 23:48:17 +0900

    [230426] PageTemplate 수정

--- src/lib/ui/Header/index.ts
+++ src/lib/ui/Header/index.ts
@@ -1,13 +1,11 @@
 import Container from './Container/Container';
 import Left from './Left/Left';
-import Logo from './Logo/Logo';
 import Right from './Right/Right';
 
 const Header = {
   Container,
   LeftSection: Left,
   RightSection: Right,
-  LogoSection: Logo,
 };
 
 export default Header;
--- src/lib/ui/PageTemplate/PageTemplate.tsx
+++ src/lib/ui/PageTemplate/PageTemplate.tsx
@@ -31,7 +31,6 @@ function PageTemplate<T extends ElementType = typeof DEFAULT_COMPONENT_ELEMENT>(
   return (
     <Element {...props} ref={ref} className={cx('template', className)}>
       <Header.Container>
-        <Header.LogoSection>Logo</Header.LogoSection>
         <Header.LeftSection>Left</Header.LeftSection>
         <Header.RightSection>Right</Header.RightSection>
       </Header.Container>
@@ -50,44 +49,3 @@ function PageTemplate<T extends ElementType = typeof DEFAULT_COMPONENT_ELEMENT>(
 }
 
 export default forwardRef(PageTemplate) as typeof PageTemplate;
-
-// import type { ReactNode } from 'react';
-
-// import Header from '@lib/ui/Header';
-// import Footer from '@lib/ui/Footer';
-// import GNB from '@lib/ui/GNB';
-
-// import type { GNBItem } from '@lib/ui/GNB/types';
-
-// import classNames from 'classnames/bind';
-// import style from './PageTemplate.module.scss';
-// const cx = classNames.bind(style);
-
-// type BaseProps = {
-//   children: ReactNode;
-//   contents?: Array<GNBItem>;
-// };
-
-// function PageTemplate({ children, contents }: BaseProps) {
-//   return (
-//     <div className={cx('template')}>
-//       <Header.Container>
-//         <Header.LogoSection>Logo</Header.LogoSection>
-//         <Header.LeftSection>Left</Header.LeftSection>
-//         <Header.RightSection>Right</Header.RightSection>
-//       </Header.Container>
-//       <div className={cx('nav-page')}>
-//         <GNB.NextTemplate contents={contents} />
-//         <div className={cx('contents')}>
-//           <div className={cx('page')}>{children}</div>
-//           <Footer.Container>
-//             <Footer.LeftSection>Left</Footer.LeftSection>
-//             <Footer.RightSection>Right</Footer.RightSection>
-//           </Footer.Container>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default PageTemplate;
