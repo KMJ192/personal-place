e32b99fc50e6a867c83347ee0028fda2f94c52f2
MyeongJunKim <myeongjun222@naver.com>
2023-05-05 15:07:36 +0900

    [230505] file upload test

--- app/lab/file/SocketUpload/SocketUpload.tsx
+++ app/lab/file/SocketUpload/SocketUpload.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { Socket, io } from 'socket.io-client';
 
@@ -8,97 +8,91 @@ import classNames from 'classnames/bind';
 import style from './SocketUpload.module.scss';
 const cx = classNames.bind(style);
 
-const socket = io('http://localhost:8081/events', {
-  autoConnect: false,
-});
-// const chunkSize = 10 * 1024; // 10kb
-const chunkSize = 100 * 1024 * 1024;
-const callback = (isEnd: DOMException | null, result: string | ArrayBuffer) => {
-  if (isEnd === null) {
-    console.log('result', result);
-  }
-};
-
 function SocketUpload() {
   const fileRef = useRef<HTMLInputElement>(null);
 
+  const [file, setFile] = useState(null);
+
+  // const chunkSize = 10 * 1024; // 10kb
+  const chunkSize = 10 * 1024 * 1024;
+
   const onClick = (e: React.MouseEvent) => {
     e.preventDefault();
+    const socket = io('http://localhost:8081/events');
 
+    socket.connect();
     socket.emit('echo', '안뇽', (chat: Socket) => {
       console.log(chat);
     });
+
+    socket.disconnect();
   };
 
-  const handleFile = (e: React.ChangeEvent) => {
-    // 파일 바뀌자 마 chunking 및 전송
+  const handleFile = async (e: React.ChangeEvent) => {
     e.stopPropagation();
-
     const fileList = fileRef.current?.files;
 
-    if (fileList && fileList[0]) {
-      const file: Blob = fileList[0];
-      const fileReader = new FileReader();
-      let offset = 0;
-      const fileSize = file.size;
+    const socket = io('http://localhost:8081/events', {
+      // autoConnect: false,
+    });
 
-      // fileReader.readAsArrayBuffer(file);
-      // fileReader.readAsDataURL(file);
+    try {
+      if (fileList && fileList[0]) {
+        const file: Blob = fileList[0];
+        console.log('start');
+        socket.emit('upload-start', {
+          name: file.name,
+          size: file.size,
+        });
+        let offset = 0;
 
-      const readSlice = (start: number, length: number) => {
-        const slice = file.slice(start, start + length);
-        fileReader.readAsArrayBuffer(slice);
-      };
+        const readSlice = (file: Blob, offset: number, length: number) => {
+          return new Promise((res, rej) => {
+            const reader = new FileReader();
+            const slice = file.slice(offset, offset + length);
 
-      // fileReader.onloadstart = () => {
-      //   // socket을 연다
-      //   console.log('start');
-      // };
+            reader.onload = (e: ProgressEvent<FileReader>) => {
+              if (e.target) {
+                res(e.target.result);
+              } else {
+                // rej('e.target is null');
+              }
+            };
 
-      // fileReader.onprogress = (e: ProgressEvent<FileReader>) => {
-      //   // 서버로 전송한다.
-      //   console.log((e.loaded * 100) / e.total);
-      // };
+            reader.readAsArrayBuffer(slice);
+          });
+        };
 
-      fileReader.onload = (e: ProgressEvent<FileReader>) => {
-        // console.log((e.loaded * 100) / e.total);
-        if (fileReader.error) {
-          console.log('error', fileReader.error);
-          return;
-        }
-
-        if (fileReader.result) {
-          callback(null, fileReader.result);
-        }
-
-        if (offset < fileSize) {
-          readSlice(offset, chunkSize);
+        const uploadSlice = async (file: Blob, offset: number) => {
+          const slice = await readSlice(file, offset, chunkSize);
+          console.log(slice);
+          socket.emit('upload-slice', {
+            slice,
+            offset,
+          });
+        };
+        while (offset < file.size) {
+          // eslint-disable-next-line no-await-in-loop
+          await uploadSlice(file, offset);
           offset += chunkSize;
         }
-      };
 
-      readSlice(offset, chunkSize);
-
-      fileReader.onerror = () => {
-        console.log('error', fileReader.error);
-      };
-
-      // fileReader.onloadend = () => {
-      //   // img.src = String(fileReader.result);
-      //   // socket을 닫는다
-      //   console.log('end');
-      // };
+        console.log('end');
+        socket.emit('upload-end');
+      }
+    } catch (e) {
+      console.log(e);
     }
   };
 
-  useEffect(() => {
-    // socket.on('message', () => {});
-    socket.connect();
-    return () => {
-      // socket.off('message', () => {});
-      socket.disconnect();
-    };
-  }, []);
+  // useEffect(() => {
+  //   // socket.on('message', () => {});
+  //   socket.connect();
+  //   return () => {
+  //     // socket.off('message', () => {});
+  //     socket.disconnect();
+  //   };
+  // }, []);
 
   return (
     <>
